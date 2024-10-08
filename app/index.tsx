e40fd8c7da9be0e5d12322235da59d02f996f86a
0rpheus-0 <erofeevvictor25@gmail.com>
2024-10-08 14:40:03 +0300

    Some steps for begin screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,62 @@
 import Logo from '@/components/Logo';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { StatusBar, Text, View } from "react-native";
+
+const PhysicalScreen = (props: View['props']) => <View
+  {...props}
+  style={{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    ...props.style as {},
+  }}
+/>
+
+const Screen = (props: View['props']) => <View
+  {...props}
+  style={{
+    flexGrow: 1,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: 'relative',
+    width: '100%',
+    ...props.style as {},
+  }}
+/>
+
+const Header = (props: View['props']) => <View
+  {...props}
+  style={{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    ...props.style as {},
+  }}
+/>
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'black',
-        justifyContent: "center",
-        alignItems: "center",
-        height: '100%',
-      }}
-    >
-      <StatusBar backgroundColor='black' />
-      <Logo />
-      <Text style={{ color: 'white' }}>Welcom!</Text>
-    </View>
+    <PhysicalScreen>
+      <StatusBar translucent={false} backgroundColor='orange' barStyle='dark-content' />
+      <Screen style={{ backgroundColor: 'black' }}>
+        <Header>
+          <Logo />
+          <Fontisto name='user-secret' color='orange' size={40} />
+        </Header>
+        <Ionicons name='accessibility' color='orange' />
+        <Text style={{ color: 'white' }}>Welcom!</Text>
+      </Screen>
+    </PhysicalScreen>
   );
 }
